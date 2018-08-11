98fdd4aafb0a065a91a6898456dcd62e1a166951
Adrien Maranville <Adrien@Maranville.io>
2018-08-11 16:10:30 -0600

    Linking outputs and setting up the controller

--- Bulls and Cows/ViewController.swift
+++ Bulls and Cows/ViewController.swift
@@ -8,15 +8,21 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
     
     @IBOutlet var tableView: NSTableView!
     @IBOutlet var lblGuess: NSTextField!
+    var answer = ""
+    var guesses = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return guesses.count
     }
 
     override var representedObject: Any? {
@@ -26,6 +32,23 @@ class ViewController: NSViewController {
     }
 
     @IBAction func btnSubmitGuess(_ sender: Any) {
+        
+    }
+    
+    func result(for guess: String) -> String {
+        return "Result"
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil }
+        if tableColumn?.title == "Guess" {
+            vw.textField?.stringValue = result(for: guesses[row])
+        }
+        return vw
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return false
     }
     
 }
